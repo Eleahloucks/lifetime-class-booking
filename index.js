dd87a1045ad280bdefed88882991071e760e5b6b
eleahmloucks <eleahloucks@gmail.com>
2023-04-26 15:41:54 -0600

    update scrape function

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const BOOKING_URL = 'https://yourgymwebsite.com/booking';
   try {
     // Your main function's code here
     // Open browser
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     // Opens a new page at the login url
     const page = await browser.newPage();
 
@@ -58,20 +58,45 @@ async function login(page, username, password) {
 async function goToClassListPage(page) {
   try {
     await page.goto(CLASSLIST_URL);
-    // may need to wait for some specific element to load before you start scraping.
-    // If that's the case, you can use page.waitForSelector() function.
-    // Example:
-    // await page.waitForSelector('.class-list-selector');
+    // Wait for a specific element that signifies the page has loaded, e.g. a class list container
+    await page.waitForSelector('.schedules-component-container');
+
+    // Take a screenshot of the class list page for testing purposes
+    await page.screenshot({ path: 'classListPage.png' });
+
+    // You can also log a message to the console to confirm that the function worked
+    console.log('Navigated to class list page successfully');
   } catch (error) {
     console.error('Error in goToClassListPage function:', error);
   }
 }
 
-async function scrapePickleballClasses(page) {
-  // Implement the scraping logic here
-  // Return an array of class information, including booking URLs
-  return [];
+async function scrapePickleballClasses(page, keywords) {
+  try {
+    // Wait for the schedule table or list to load
+    await page.waitForSelector('.schedule-row');
+
+    // Scrape the schedule data and find classes with the specified keywords in the title
+    const classes = await page.evaluate((keywords) => {
+      const rows = Array.from(document.querySelectorAll('.schedule-row'));
+      return rows
+        .map((row) => {
+          const name = row.querySelector('.class-name')?.textContent.trim();
+          const time = row.querySelector('.class-time')?.textContent.trim();
+          const bookingUrl = row.querySelector('.booking-link')?.href;
+
+          return { name, time, bookingUrl };
+        })
+        .filter((classInfo) => keywords.some((keyword) => classInfo.name.includes(keyword)));
+    }, keywords);
+
+    return classes;
+  } catch (error) {
+    console.error('Error in scrapeClassesWithKeywords function:', error);
+    return [];
+  }
 }
+
 
 async function bookClass(page, classInfo) {
   // Implement the class booking logic here
